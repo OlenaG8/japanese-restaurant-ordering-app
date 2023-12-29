2b916bd201397925606672818919a72faf1e50b6
Olena <135965492+OlenaG8@users.noreply.github.com>
2023-12-29 21:55:23 +0100

    Creating a basic skeleton of the app

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Paitan Ramen",
+        ingredients: ["creamy chicken broth", "thick ramen noodles", "karaage chicken", "baked tomatoes", "coriander", "citrus oil"],
+        id: 0,
+        price: 15,
+        dishPhoto: `assets/ramen.jpg`,
+    },
+    {
+        name: "Sushi set (25 pieces)",
+        ingredients: ["5 pcs nigiri", "5 pcs tiger roll", "5 pcs goma maki", "5 pcs eel maki", "5 pcs hosomaki"],
+        price: 25,
+        dishPhoto: `assets/sushi.jpg`,
+        id: 1
+    },
+        {
+        name: "Traditional matcha",
+        ingredients: ["green powdered Japanese tea of the highest quality"],
+        price: 6,
+        dishPhoto: `assets/matcha.jpg`,
+        id: 2
+    }
+]
