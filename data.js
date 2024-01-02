92102d1e5ab8005167be3dabc3ea647e5194b0bc
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-01-02 19:09:41 +0100

    Added menu categories

--- data.js
+++ data.js
@@ -5,19 +5,31 @@ export const menuArray = [
         id: 0,
         price: 15,
         itemPhoto: `assets/ramen.jpg`,
+        type: "ramens"
     },
     {
         name: "Sushi set (25 pieces)",
         ingredients: ["5 pcs nigiri", "5 pcs tiger roll", "5 pcs goma maki", "5 pcs eel maki", "5 pcs hosomaki"],
         price: 25,
         itemPhoto: `assets/sushi.jpg`,
-        id: 1
+        id: 1,
+        type: "sushi"
     },
         {
         name: "Traditional matcha",
         ingredients: ["green powdered Japanese tea of the highest quality"],
         price: 6,
         itemPhoto: `assets/matcha.jpg`,
-        id: 2
+        id: 2,
+        type: "drinks"
+    },
+    {
+        name: "Tonkotsu ramen",
+        ingredients: ["creamy pork broth", "thin hakata noodles", "caramelized chashu", "corn", "chicken oil"],
+        price: 16,
+        itemPhoto: `assets/ramen2.jpg`,
+        id: 2,
+        type: "ramens"
     }
+
 ]
--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 import { menuArray } from './data.js'
 
-function getFeedHtml() {
-    let feedHtml = ``
+const addBtn = document.getElementById('add_btn')
 
-    menuArray.forEach(function(item){
-        feedHtml += `
-            <div class="menu-item">
-                <div class="item-inner">
-                    <div class="item-photo">
-                        <img src="${item.itemPhoto}" class="dish-preview" alt="${item.name}">
-                    </div>
-                    <div class="menu-details">
-                        <h3>${item.name}</h3>
-                        <p>${item.ingredients}</p>
-                        <h4>${item.price}€</h4>
-                    </div> 
-                    <div class="quantityBtn">
-                        <button class="addBtn">+</button>
-                        <p class="hidden">0</p>
-                        <button class="removeBtn hidden">-</button>
-                    </div>        
-                </div>  
-            </div>
-            `
+document.addEventListener('click', function(e){
+    if (e.target.dataset.add) {
+        handleAddToCardBtn(e.target.dataset.add)
+    }
+})
 
-    })
-    return feedHtml 
+function handleAddToCardBtn(itemId) {
+    document.getElementById('remove_btn').classList.toggle('hidden')
+    console.log("kupa")
+    const targetItemObj = menuArray.filter(function(item){
+        return item.id = itemId
+    })[0]
+}
+
+
+
+function getFeedHtml(type) {
+    return menuArray.filter(item => item.type === type).map(getItemHtml).join('')
+}
+
+function getItemHtml(item) {
+    return `<div class="menu-item">
+    <div class="item-inner">
+        <div class="item-photo">
+            <img src="${item.itemPhoto}" class="dish-preview" alt="${item.name}">
+        </div>
+        <div class="menu-details">
+            <h3>${item.name}</h3>
+            <p>${item.ingredients.join(', ')}</p>
+            <h4>${item.price}€</h4>
+        </div> 
+        <div class="quantity_btn">
+            <button class="add_btn" id="add_btn">+</button>
+            <p class="hidden">0</p>
+            <button class="remove_btn hidden">-</button>
+        </div>        
+    </div>  
+</div>
+`
 }
 
 function render(){
-    document.getElementById('menu-container').innerHTML = getFeedHtml()
+        for (let type of ['ramens', 'sushi', 'drinks']) {
+            document.getElementById(type).innerHTML += getFeedHtml(type)
+        }
 }
 
 render()
