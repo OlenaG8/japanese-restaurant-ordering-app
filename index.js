9d8bf544586f88937c3c1ebfcd7a5a90f55aa4df
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-01-02 17:05:33 +0100

    Added 'add to card' button

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ function getFeedHtml() {
             <div class="menu-item">
                 <div class="item-inner">
                     <div class="item-photo">
-                        <img src="${item.itemPhoto}" class="dish-preview">
-                    <div>
+                        <img src="${item.itemPhoto}" class="dish-preview" alt="${item.name}">
+                    </div>
                     <div class="menu-details">
                         <h3>${item.name}</h3>
                         <p>${item.ingredients}</p>
                         <h4>${item.price}€</h4>
-                    </div>
-                        </div>   
-                    </div>            
+                    </div> 
+                    <div class="quantityBtn">
+                        <button class="addBtn">+</button>
+                        <p class="hidden">0</p>
+                        <button class="removeBtn hidden">-</button>
+                    </div>        
                 </div>  
             </div>
             `
