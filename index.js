cc12d97432c3e071f8a1f21bffda71d21d4d8655
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-01-05 23:30:37 +0100

    Buttons fixed

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ document.addEventListener('click', function(e){
 })
 
 function handleAddBtn(itemId){
-    document.querySelectorAll(`#quantity-${itemId}`).forEach(operator => operator.style.display = 'visible')
-    const targetItem = menuArray.filter(function(item){
+    const targetItem = menuArray.find(function(item){
         return item.id === parseInt(itemId)
-    })[0]
+    })
     targetItem.amount++
     render()
 }
 
 
 function handleRemoveBtn(itemId){
-    const targetItem = menuArray.filter(function(item){
+    const targetItem = menuArray.find(function(item){
         return item.id === parseInt(itemId)
-    })[0]
+    })
 
     if (targetItem.amount >= 1) {
         targetItem.amount--
     }
+
     render()
 }
 
@@ -35,6 +35,8 @@ function getFeedHtml(type) {
 }
 
 function getItemHtml(item) {
+    const buttonsVisibility = item.amount === 0 ? 'hidden' : ''
+    
     return `<div class="menu-item">
     <div class="item-inner">
         <div class="item-photo">
@@ -47,8 +49,8 @@ function getItemHtml(item) {
         </div> 
         <div class="btn_container">
             <button class="quantity_btn" data-add="${item.id}">+</button>
-            <p id="quantity-${item.id}">${item.amount}</p>
-            <button class="quantity_btn" id="quantity-${item.id}" data-remove="${item.id}">-</button>
+            <p class="${buttonsVisibility} quantity-${item.id}">${item.amount}</p>
+            <button class="${buttonsVisibility} quantity_btn quantity-${item.id}" data-remove="${item.id}">-</button>
         </div>        
     </div>  
 </div>
